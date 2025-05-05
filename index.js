928f540ee4a941c303ff5ece93274215135f14ee
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2025-05-05 11:38:50 -0400

    install and init graphql

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import routes from './src/routes/crmRoutes';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+// the root provides a resolver function for each API endpoint
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    }
+};
 
 const app = express();
 const PORT = 3000;
 
-// Set strictQuery option
-mongoose.set('strictQuery', true);
-
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb');
-
-//bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 // serving static files
 app.use(express.static('public'));
 
-
-routes(app);
-
-app.get('/', (req, res) => 
-    res.send(`Node and express server is running on port ${PORT}`)
-);
+// Setup graphql endpoint
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true // Enable GraphiQL IDE
+}));
 
 app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
+    console.log(`Your server is running on port ${PORT}/graphql`)
 );
